4eff40d8a7f9db3d1d7a4edb60881d4acbb7d465
Shubhang Desai <shubhang.desai@gmail.com>
2019-02-04 03:31:22 -0800

    add dice loss

--- train.py
+++ train.py
@@ -20,15 +20,17 @@ def get_parser():
     parser.add_argument('--depth', default=4, type=int)
     parser.add_argument('--num_classes', default=2, type=int)
     parser.add_argument('--lr', default=1e-4, type=float)
+    parser.add_argument('--use_dice_loss', action='store_true')
     parser.add_argument('--batch_size', default=1, type=int)
     parser.add_argument('--num_epochs', default=50, type=int)
 
     return parser
 
-def save_images(X, pred, args, e):
-    X, pred = X.cpu().data.numpy(), pred.cpu().data.numpy()
+def save_images(X, y, pred, args, e):
+    X, y, pred = X.cpu().data.numpy(), y.squeeze(0).cpu().data.numpy(), pred.cpu().data.numpy()
 
     scipy.misc.imsave('exps/' + args.name + '/results/' + str(e) + '_X.jpg', np.transpose(X, (1, 2, 0)))
+    scipy.misc.imsave('exps/' + args.name + '/results/' + str(e) + '_y.jpg', y)
     scipy.misc.imsave('exps/' + args.name + '/results/' + str(e) + '_pred.jpg', np.argmax(pred, axis=0))
 
 def graph_loss(losses, name):
@@ -36,6 +38,15 @@ def graph_loss(losses, name):
     plt.plot(list(range(len(losses))), losses)
     plt.savefig('exps/' + name + '/loss.png')
     plt.close()
+
+def dice_loss(pred_flat, y_flat):
+    smooth = 1.
+    pred_flat = torch.sigmoid(pred_flat.squeeze(1))
+
+    intersection = (pred_flat * y_flat).sum()
+    union = pred_flat.sum() + y_flat.sum()
+    
+    return 1. - ((2. * intersection + smooth) / (union + smooth))
 
 def train(args):
     model = UNet(in_channels=args.in_channels, n_classes=args.num_classes, depth=args.depth, padding=True, up_mode='upsample').to(device)
@@ -47,10 +58,11 @@ def train(args):
         total_loss = 0
         for i, (X, y) in enumerate(loader):
             optimizer.zero_grad()
-            X, y = X.to(device), y.to(device).reshape(args.batch_size, -1).long()  # [N, in_channels, H, W], [N, HW]
+            X, y = X.to(device), y.to(device)
 
-            pred = model(X)  # [N, num_classes, H, W]
-            loss = F.cross_entropy(pred.reshape(args.batch_size, args.num_classes, -1), y)
+            pred = model(X) 
+            y_flat, pred_flat = y.reshape(args.batch_size, -1), pred.reshape(args.batch_size, args.num_classes, -1)
+            loss = dice_loss(pred_flat, y_flat) if args.use_dice_loss else F.cross_entropy(pred_flat, y_flat.long())
 
             loss.backward()
             optimizer.step()
@@ -59,7 +71,7 @@ def train(args):
             sys.stdout.write('\rEpoch %i: %i/%i' % (e+1, i+1, len(loader)))
 
         torch.save(model.state_dict(), 'exps/' + args.name + '/checkpoints/' + str(e))
-        save_images(X[0], pred[0], args, e)
+        save_images(X[0], y[0], pred[0], args, e)
 
         losses.append(total_loss / len(loader))
         graph_loss(losses, args.name)
